435a76f1859d2446eab479f9f5a611272cb92378
Leon Sick <le161sic@htwg-konstanz.de>
2020-11-28 11:49:07 +0100

    limit choices for flags

--- capture_images.py
+++ capture_images.py
@@ -6,9 +6,9 @@ import uuid
 
 parser = argparse.ArgumentParser(description='Take a specific number of images with a pause of 3 seconds.')
 parser.add_argument('-n', '--number', type=int, help='`Specifiy the number of images you want to take.', required=True)
-parser.add_argument('-ic', '--iclass', help='`Specifiy the class of images you want to take.', required=True)
-parser.add_argument('-ds', '--dataset', help='`Specifiy the class of images you want to take.', required=True)
-parser.add_argument('-s', '--size', ngars="?", default="large", help='`Specifiy the class of images you want to take.', required=True)
+parser.add_argument('-ic', '--iclass', help='`Specifiy the class of images you want to take.', choices=['apple', 'banana', 'broccoli', 'paprika', 'aubergine', 'cucumber', 'orange', 'pear'], required=True)
+parser.add_argument('-ds', '--dataset', help='`Specifiy the class of images you want to take.', choices=['train', 'test'], required=True)
+parser.add_argument('-s', '--size', ngars="?", default="large", help='`Specifiy the class of images you want to take.', choices=['large', 'medium', 'small'])
 
 args = parser.parse_args()
 number_of_images = args.number
@@ -35,6 +35,8 @@ while img_counter < number_of_images:
 		break
 	if size == "small":
 		image_resized = cv2.resize(image, (300, 300))
+	else if size == "medium":
+		image_resized = cv2.resize(image, (608, 608))
 	else:
 		image_resized = cv2.resize(image, (1080, 720))
 	cv2.imshow("Press SPACE to save image", image_resized)
