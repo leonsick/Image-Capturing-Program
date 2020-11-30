0d43046406630ae97929a3cbb31136ccec8452db
Leon Sick <le161sic@htwg-konstanz.de>
2020-11-30 08:13:35 +0100

    adding random functionality

--- capture_images.py
+++ capture_images.py
@@ -2,13 +2,14 @@ import os
 import argparse
 import cv2
 import uuid
+import random
 
 
 parser = argparse.ArgumentParser(description='Take a specific number of images with a pause of 3 seconds.')
 parser.add_argument('-n', '--number', type=int, help='`Specifiy the number of images you want to take.', required=True)
 parser.add_argument('-ic', '--iclass', help='`Specifiy the class of images you want to take.', choices=['apple', 'banana', 'broccoli', 'paprika', 'aubergine', 'cucumber', 'orange', 'pear'], required=True)
 parser.add_argument('-ds', '--dataset', help='`Specifiy the class of images you want to take.', choices=['train', 'test'], required=True)
-parser.add_argument('-s', '--size', default="large", help='`Specifiy the class of images you want to take.', choices=['large', 'medium', 'small'])
+parser.add_argument('-s', '--size', default="large", help='Specifiy the class of images you want to take. If no param is chosen, it will be random between small, medium and large', choices=['large', 'medium', 'small', 'random'])
 
 args = parser.parse_args()
 number_of_images = args.number
@@ -28,6 +29,16 @@ while img_counter < number_of_images:
 	if not status:
 		print("Image capture failed")
 		break
+	if size == "random":
+		rand = random.randint(1, 3)
+		print(rand)
+		if rand == 1:
+			size = "small"
+		if rand == 2:
+			size = "medium"
+		if rand == 3:
+			size = "large"
+		print(size)
 	if size == "small":
 		image_resized = cv2.resize(image, (300, 300))
 	elif size == "medium":
